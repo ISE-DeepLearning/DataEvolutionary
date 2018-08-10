ccf3d2bdab6ec7880848619bb7dec2a2f5ec7705
AdamKingsley <desperatewilder@gmail.com>
2018-08-10 15:27:09 +0800

    cifar-10 为 cifar10

--- config.py
+++ config.py
@@ -5,20 +5,20 @@ datasets = {
         'training_dir_path': './mnist/training_npy/',
         'test_dir_path': './mnist/test_npy/'
     },
-    'cifar-10': {
-        'origin_dir_path': './cifar-10/cifar-10-batches-py/',
-        'training_dir_path': './cifar-10/training_npy/',
-        'test_dir_path': './cifar-10/test_npy/'
+    'cifar10': {
+        'origin_dir_path': './cifar10/cifar10-batches-py/',
+        'training_dir_path': './cifar10/training_npy/',
+        'test_dir_path': './cifar10/test_npy/'
     }
 }
 
 exp = {
     'exp_dir_path': './exp/',
-    'exp_index': 'exp2',
-    'dataset': 'cifar-10',
+    'exp_index': 'exp1',
+    'dataset': 'cifar10',
     'shape': (3, 32, 32),
     'label_num': 10,
-    'sample_every_label': 10,
+    'sample_every_label': 5,
     'upper': 255,
     'mode': ['all'],
     'repeat_times': 5
